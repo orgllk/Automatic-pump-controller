e81e455be6fd69a312e30fa97a3554b23e6aa74a
ORGLLK <jagatjyothirmoyorgllk@gmail.com>
2026-04-19 00:42:56 +0530

    Temp protection

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,68 @@
 //BY ORGLLK
 //XXXXXXXXXXXXXXXXXXXXXXX//
 #include <Arduino.h>
+#include"DHT.h"
 // Ultrasonic sensor pins////////////////////
-#define ultt1       22     // Trigger pin for sensor 1
-#define ultr1       23     // Echo pin for sensor 1
-#define ultt2       24     // Trigger pin for sensor 2
-#define ultr2       25     // Echo pin for sensor 2
+#define ultt1       22    // Trigger pin for sensor 1
+#define ultr1       23    // Echo pin for sensor 1
+#define ultt2       24    // Trigger pin for sensor 2
+#define ultr2       25    // Echo pin for sensor 2
 //////////////////////////////////////////
-// Relay and indicators//  //
-#define relay       10     //Relay pin for controlling the motor
-#define relaystats  11     // ``Relay status led for indicating motor state
-/////Main SW&LED/////      //
-#define mainswitch  12     //Main switch pin for turning on/off the system
-#define mainled     13     //Main power led indicator
-//////////////////         // 
-//warning system//         //
-#define senserled   26     //sensor issue led for warning system
-#define falutled    27     //Lerd for fault detection
-#define elecprob    28     // Electric problem pin for fault detection
-
+// Relay and indicators// //
+#define relay       10    //Relay pin for controlling the motor
+#define relaystats  11    // ``Relay status led for indicating motor state
+/////Main SW&LED/////     //
+#define mainswitch  8     //Main switch pin for turning on/off the system
+#define mainled     9     //Main power led indicator
+//////////////////        // 
+//warning system//        //
+#define senserled   5     //sensor issue led for warning system
+#define falutled    7     //Lerd for fault detection
+#define elecprob    27    // Electric problem pin for fault detection
+#define reset       52    // DEV
+#define DHTPIN      26    // GPIO pin for DHT11 data
+DHT dht;            //    // Create DHT object
+#define overheatp   6     // Overheat cut off protection
 void setup() {
   Serial.begin(9600);
 
   // Ultrasonic pins
-  pinMode(ultt1, OUTPUT);
-  pinMode(ultr1, INPUT);
-  pinMode(ultt2, OUTPUT);
-  pinMode(ultr2, INPUT);
-
+  pinMode(ultt1,      OUTPUT);
+  pinMode(ultr1,      INPUT);
+  pinMode(ultt2,      OUTPUT);
+  pinMode(ultr2,      INPUT);
+                      
   // Relay and indicators
-  pinMode(relay, OUTPUT);
+  pinMode(relay,      OUTPUT);
   pinMode(relaystats, OUTPUT);
+  pinMode(mainled,    OUTPUT);
   pinMode(mainswitch, INPUT);
-  pinMode(mainled, OUTPUT);
+
   ///////warning system//////
-  pinMode(falutled, OUTPUT);
-  pinMode(senserled, OUTPUT);
-  pinMode(elecprob, INPUT);
+  pinMode(overheatp,  OUTPUT);
+  pinMode(falutled,   OUTPUT);
+  pinMode(senserled,  OUTPUT);
+  pinMode(elecprob,   INPUT);
+  pinMode(reset,      INPUT); // Reset button with pull-up resistor
+  dht.setup(DHTPIN);  // Initialize sensor on defined pin
 }
 
 void loop() {
+  ///////////////////////////TEMP/////////////////////////////////////////////////
+  delay(dht.getMinimumSamplingPeriod());  // Respect sensor timing
+  float temperatureC = dht.getTemperature();   // Read temperature in Celsius
+  /////                           x                                  ////////////
+  if(temperatureC >=70 ){
+    digitalWrite(overheatp,LOW);
+  }else{
+    digitalWrite(overheatp,HIGH);
+  }
+  ///////////////////////////TEMP////////////////////////////////////////////////
+  ////////                   X                                       ///////////
+  if(digitalRead(reset) == HIGH) { // Check if reset button is pressed
+    digitalWrite(senserled, LOW);    // Sensor issue LED OFF
+    digitalWrite(falutled, LOW);     // Fault LED OFFe
+  }
   // Trigger sensor 1
   digitalWrite(ultt1, LOW);
   delayMicroseconds(2);
@@ -54,7 +77,10 @@ void loop() {
   long duration1 = pulseIn(ultr1, HIGH);
   int distance1 = duration1 * 0.034 / 2;
   Serial.print("Distance1: ");
-  Serial.println(distance1);
+  Serial.print(distance1);
+  Serial.print("    TEMP: ");
+  Serial.println(temperatureC, 1);
+
 
   // Trigger sensor 2
   digitalWrite(ultt2, LOW);
@@ -67,19 +93,21 @@ void loop() {
   long duration2 = pulseIn(ultr2, HIGH);
   int distance2 = duration2 * 0.034 / 2;
   Serial.print("Distance2: ");
-  Serial.println(distance2);
+  Serial.print(distance2);
+  Serial.print("     TEMP: ");
+  Serial.println(temperatureC, 1);
 
 if(digitalRead(mainswitch) == HIGH){
   if(digitalRead(elecprob) == HIGH) {
     digitalWrite(relay, LOW);        // Motor OFF
-    Serial.println("Fault detected, motor OFF");
+    Serial.println("Fault detected, ");
     digitalWrite(relaystats, LOW);
     digitalWrite(falutled, HIGH); // Warning ON
   }else {
-      digitalWrite(falutled, LOW); // Warning OFF
-      if(distance1 >= 84 && distance2 >= 84 && distance1 <= 18 && distance2 <= 18){
-      digitalWrite(senserled, HIGH); // Warning ON
-      Serial.println("senser issue"); 
+      if(distance1 < 18 || distance1 > 81 || distance2 < 18 || distance2 > 81){
+       digitalWrite(senserled, HIGH); // Warning ON
+       digitalWrite(falutled, HIGH); // Warning ON                                    
+       Serial.println("senser issue"); 
       }else{
          // / Control logic
           if(distance1 <= 5 && distance2 <= 5) {
@@ -95,4 +123,5 @@ if(digitalRead(mainswitch) == HIGH){
      } 
    }
  }
+ delay(500); // Delay for stability
 }
